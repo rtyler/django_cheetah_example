6452197a60082456ba92391265245f5128efb5d0
R. Tyler Ballance <tyler@monkeypox.org>
2009-12-26 11:55:50 -0800

    Add Cheetah templates dir

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Django settings for django_cheetah_example project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -69,6 +70,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.abspath('Cheetar/templates'),
 )
 
 INSTALLED_APPS = (
